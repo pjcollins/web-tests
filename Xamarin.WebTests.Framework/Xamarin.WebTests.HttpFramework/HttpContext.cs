66d8bfd8faec641d414a8e972824aa1d9ec36c4d
Martin Baulig <mabaul@microsoft.com>
2017-07-26 16:36:26 -0400

    Y

---
+++ Xamarin.WebTests.Framework/Xamarin.WebTests.HttpFramework/HttpContext.cs
@@ -0,0 +1,203 @@
+﻿//
+// HttpContext.cs
+//
+// Author:
+//       Martin Baulig <mabaul@microsoft.com>
+//
+// Copyright (c) 2017 Xamarin Inc. (http://www.xamarin.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Xamarin.AsyncTests;
+
+namespace Xamarin.WebTests.HttpFramework
+{
+	class HttpContext : IDisposable
+	{
+		public HttpServer Server {
+			get;
+		}
+
+		public HttpConnection Connection {
+			get { return connection; }
+		}
+
+		public bool ReusingConnection {
+			get;
+		}
+
+		static int nextID;
+		public readonly int ID = Interlocked.Increment (ref nextID);
+
+		internal string ME {
+			get;
+		}
+
+		public HttpContext (HttpConnection connection, bool reused)
+		{
+			this.connection = connection;
+			Server = connection.Server;
+			ReusingConnection = reused;
+
+			serverStartTask = new TaskCompletionSource<object> ();
+			serverReadyTask = new TaskCompletionSource<object> ();
+			ME = $"[{ID}:{GetType ().Name}:{connection.ME}:{reused}]";
+		}
+
+		HttpConnection connection;
+		TaskCompletionSource<object> serverReadyTask;
+		TaskCompletionSource<object> serverStartTask;
+
+		public Task ServerStartTask => serverStartTask.Task;
+
+		public Task ServerReadyTask => serverReadyTask.Task;
+
+		async Task<(bool complete, bool success)> Initialize (
+			TestContext ctx, HttpOperation operation, CancellationToken cancellationToken)
+		{
+			try {
+				(bool complete, bool success) result;
+				if (ReusingConnection) {
+					if (await ReuseConnection (ctx, cancellationToken).ConfigureAwait (false))
+						result = (true, true);
+					else
+						result = (false, false);
+				} else {
+					if (await InitConnection (ctx, operation, cancellationToken).ConfigureAwait (false))
+						result = (true, true);
+					else
+						result = (true, false);
+				}
+				serverReadyTask.TrySetResult (null);
+				return result;
+			} catch (OperationCanceledException) {
+				OnCanceled ();
+				throw;
+			} catch (Exception ex) {
+				OnError (ex);
+				throw;
+			}
+		}
+
+		void OnCanceled ()
+		{
+			serverStartTask.TrySetCanceled ();
+			serverReadyTask.TrySetCanceled ();
+		}
+
+		void OnError (Exception error)
+		{
+			serverStartTask.TrySetException (error);
+			serverReadyTask.TrySetResult (error);
+		}
+
+		async Task<bool> ReuseConnection (TestContext ctx, CancellationToken cancellationToken)
+		{
+			var me = $"{ME}({connection.ME}) REUSE";
+			ctx.LogDebug (2, $"{me}");
+
+			serverStartTask.TrySetResult (null);
+
+			cancellationToken.ThrowIfCancellationRequested ();
+			var reusable = await connection.ReuseConnection (ctx, cancellationToken).ConfigureAwait (false);
+
+			ctx.LogDebug (2, $"{me} #1: {reusable}");
+			return reusable;
+		}
+
+		async Task<bool> InitConnection (TestContext ctx, HttpOperation operation, CancellationToken cancellationToken)
+		{
+			var me = $"{ME}({connection.ME}) INIT";
+			ctx.LogDebug (2, $"{me}");
+
+			cancellationToken.ThrowIfCancellationRequested ();
+			var acceptTask = connection.AcceptAsync (ctx, cancellationToken);
+
+			serverStartTask.TrySetResult (null);
+
+			await acceptTask.ConfigureAwait (false);
+
+			ctx.LogDebug (2, $"{me} ACCEPTED {connection.RemoteEndPoint}");
+
+			bool haveRequest;
+
+			cancellationToken.ThrowIfCancellationRequested ();
+			try {
+				await connection.Initialize (ctx, operation, cancellationToken);
+				ctx.LogDebug (2, $"{me} #1 {connection.RemoteEndPoint}");
+
+				if (operation != null && operation.HasAnyFlags (HttpOperationFlags.ServerAbortsHandshake))
+					throw ctx.AssertFail ("Expected server to abort handshake.");
+
+				/*
+				 * There seems to be some kind of a race condition here.
+				 *
+				 * When the client aborts the handshake due the a certificate validation failure,
+				 * then we either receive an exception during the TLS handshake or the connection
+				 * will be closed when the handshake is completed.
+				 *
+				 */
+				haveRequest = await connection.HasRequest (cancellationToken);
+				ctx.LogDebug (2, $"{me} #2 {haveRequest}");
+
+				if (operation != null && operation.HasAnyFlags (HttpOperationFlags.ClientAbortsHandshake))
+					throw ctx.AssertFail ("Expected client to abort handshake.");
+			} catch (Exception ex) {
+				if (operation.HasAnyFlags (HttpOperationFlags.ServerAbortsHandshake, HttpOperationFlags.ClientAbortsHandshake))
+					return false;
+				ctx.LogDebug (2, $"{me} FAILED: {ex.Message}\n{ex}");
+				throw;
+			}
+
+			if (!haveRequest) {
+				ctx.LogMessage ($"{me} got empty requets!");
+				throw ctx.AssertFail ("Got empty request.");
+			}
+
+			if (Server.UseSSL) {
+				ctx.Assert (connection.SslStream.IsAuthenticated, "server is authenticated");
+				if (operation != null && operation.HasAnyFlags (HttpOperationFlags.RequireClientCertificate))
+					ctx.Assert (connection.SslStream.IsMutuallyAuthenticated, "server is mutually authenticated");
+			}
+
+			ctx.LogDebug (2, $"{me} DONE");
+			return true;
+		}
+
+		int disposed;
+
+		void Close ()
+		{
+			if (connection != null) {
+				connection.Dispose ();
+				connection = null;
+			}
+		}
+
+		public void Dispose ()
+		{
+			if (Interlocked.CompareExchange (ref disposed, 1, 0) != 0)
+				return;
+
+			Close ();
+		}
+	}
+}
