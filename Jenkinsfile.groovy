a92e819fbc17f26b10ab941f53bdba32c39ab49c
Martin Baulig <martin.baulig@xamarin.com>
2018-02-21 14:25:59 -0500

    X

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -52,6 +52,8 @@ def provision ()
 	def summary = readFile summaryFile
 	echo "Setting build summary: $summary"
 	currentBuild.description = summary
+    manager.addShortText (summary)
+	
 }
 
 def enableMono ()
