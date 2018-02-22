1c95971ba827f99ed66121bda86507d2cb03edde
Martin Baulig <martin.baulig@xamarin.com>
2018-02-22 18:49:52 -0500

    X

--- Jenkinsfile-trigger.groovy
+++ Jenkinsfile-trigger.groovy
@@ -118,7 +118,7 @@ def triggerJob ()
 
 def slackSend (String color, String message)
 {
-	slackSend channel: "#martin-jenkins", color: color, message: "${currentBuild.displayName}: ${message} (<${env.BUILD_URL}|Open>)\n${currentBuild.description}"
+	slackSend channel: "#martin-jenkins", color: color, message: "${env.JOB_NAME} ${currentBuild.displayName}: ${message} (<${env.BUILD_URL}|Open>)\n${currentBuild.description}"
 }
 
 node ('felix-25-sierra') {
